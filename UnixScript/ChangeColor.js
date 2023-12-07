54560d4626e70beac6c1805cf8d397890e18a825
Jayda Pizzuco <jaydapizzuco@gmail.com>
2023-12-07 00:28:35 -0500

    Added script to change colours. Added screenshots

---
+++ UnixScript/ChangeColor.js
@@ -0,0 +1,8 @@
+function changeColor() {
+
+    var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+    document.body.style.backgroundColor = randomColor;
+  }
+  
+  changeColor();
